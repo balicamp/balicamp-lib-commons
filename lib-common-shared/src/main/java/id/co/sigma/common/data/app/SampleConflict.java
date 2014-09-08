7f208dc89b34f788869a606474aadb7dbff8071f
agung3ka <agung3ka@gmail.com>
2014-09-08 11:25:45 +0800

    tester class untuk konflik

---
+++ lib-common-shared/src/main/java/id/co/sigma/common/data/app/SampleConflict.java
@@ -0,0 +1,14 @@
+package id.co.sigma.common.data.app;
+
+/**
+ * 
+ * @author <a href="mailto:gusti.darmawan@sigma.co.id">Eka Darmawan</a>
+ */
+public class SampleConflict {
+	
+	
+	
+	public String getMessage () {
+		return "hello world";
+	}
+}
