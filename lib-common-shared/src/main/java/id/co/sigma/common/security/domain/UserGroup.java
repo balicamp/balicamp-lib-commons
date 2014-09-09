01e098ede9410046fafbd6dd24e468a117671ec0
goesderai <goesderai@gmail.com>
2014-09-09 11:13:29 +0800

    penambahan implements SingleKeyEntityData

--- lib-common-shared/src/main/java/id/co/sigma/common/security/domain/UserGroup.java
+++ lib-common-shared/src/main/java/id/co/sigma/common/security/domain/UserGroup.java
@@ -5,13 +5,10 @@
  */
 package id.co.sigma.common.security.domain;
 
-import id.co.sigma.common.data.app.SimpleDualControlData;
+import id.co.sigma.common.data.SingleKeyEntityData;
+import id.co.sigma.common.security.domain.audit.BaseAuditedObject;
+import id.co.sigma.common.util.json.IJSONFriendlyObject;
 import id.co.sigma.common.util.json.ParsedJSONContainer;
-
-
-
-
-import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -31,7 +28,7 @@ import javax.persistence.Table;
  */
 @Entity
 @Table(name="sec_group")
-public class UserGroup extends SimpleDualControlData<UserGroup>{
+public class UserGroup extends BaseAuditedObject implements SingleKeyEntityData<Long>, IJSONFriendlyObject<UserGroup>{
 
 	private static final long serialVersionUID = 8417245322597100469L;
 	
@@ -41,8 +38,9 @@ public class UserGroup extends SimpleDualControlData<UserGroup>{
 	**/
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
-	@Column(name="pk" , nullable=false )
+	@Column(name="group_id")
 	private Long id;
+	
 	/**
 	* id applikasi<br/>
 	* column :APPLICATION_ID
@@ -60,24 +58,27 @@ public class UserGroup extends SimpleDualControlData<UserGroup>{
 	**/
 	@Column(name="group_code" , length=32, nullable=false)
 	private String groupCode;
+	
 	/**
 	* nama group<br/>
 	* column :GROUP_NAME
 	**/
 	@Column(name="group_name" , length=128, nullable=false)
 	private String groupName;
+	
 	/**
 	* Y=Yes, N=No<br/>
 	* column :is_super_group
 	**/
 	@Column(name="is_super_group", length=1)
-	private String superGroup;
+	private String superGroup = "N";
+	
 	/**
 	* status<br/>
 	* column :data_status
 	**/
-	@Column(name="active_flag",length=1)
-	private String activeFlag ="A";
+	@Column(name="data_status",length=1)
+	private String activeFlag = "A";
 
 	/**
 	* group id<br/>
@@ -86,6 +87,7 @@ public class UserGroup extends SimpleDualControlData<UserGroup>{
 	public void setId(Long id){
 	  this.id=id;
 	}
+	
 	/**
 	* group id<br/>
 	* column :GROUP_ID
@@ -93,6 +95,7 @@ public class UserGroup extends SimpleDualControlData<UserGroup>{
 	public Long getId(){
 	    return this.id;
 	}
+	
 	/**
 	* id applikasi<br/>
 	* column :APPLICATION_ID
@@ -100,6 +103,7 @@ public class UserGroup extends SimpleDualControlData<UserGroup>{
 	public void setApplicationId(Long applicationId){
 	  this.applicationId=applicationId;
 	}
+	
 	/**
 	* id applikasi<br/>
 	* column :APPLICATION_ID
@@ -107,6 +111,7 @@ public class UserGroup extends SimpleDualControlData<UserGroup>{
 	public Long getApplicationId(){
 	    return this.applicationId;
 	}
+	
 	/**
 	 * reference ke object Application {@link id.co.sigma.common.security.domain.Application}
 	 * @return application
@@ -114,6 +119,7 @@ public class UserGroup extends SimpleDualControlData<UserGroup>{
 	public Application getApplication() {
 		return application;
 	}
+	
 	/**
 	 * reference ke object Application {@link id.co.sigma.common.security.domain.Application}
 	 * @param application
@@ -121,6 +127,7 @@ public class UserGroup extends SimpleDualControlData<UserGroup>{
 	public void setApplication(Application application) {
 		this.application = application;
 	}
+	
 	/**
 	* kode group.unik dalam 1 app<br/>
 	* column :GROUP_CODE
@@ -128,6 +135,7 @@ public class UserGroup extends SimpleDualControlData<UserGroup>{
 	public void setGroupCode(String groupCode){
 	  this.groupCode=groupCode;
 	}
+	
 	/**
 	* kode group.unik dalam 1 app<br/>
 	* column :GROUP_CODE
@@ -135,6 +143,7 @@ public class UserGroup extends SimpleDualControlData<UserGroup>{
 	public String getGroupCode(){
 	    return this.groupCode;
 	}
+	
 	/**
 	* nama group<br/>
 	* column :GROUP_NAME
@@ -142,6 +151,7 @@ public class UserGroup extends SimpleDualControlData<UserGroup>{
 	public void setGroupName(String groupName){
 	  this.groupName=groupName;
 	}
+	
 	/**
 	* nama group<br/>
 	* column :GROUP_NAME
@@ -149,6 +159,7 @@ public class UserGroup extends SimpleDualControlData<UserGroup>{
 	public String getGroupName(){
 	    return this.groupName;
 	}
+	
 	/**
 	* Y=Yes, N=No<br/>
 	* column :IS_SUPER_GROUP
@@ -156,6 +167,7 @@ public class UserGroup extends SimpleDualControlData<UserGroup>{
 	public void setSuperGroup(String superGroup){
 	  this.superGroup=superGroup;
 	}
+	
 	/**
 	* Y=Yes, N=No<br/>
 	* column :IS_SUPER_GROUP
@@ -163,7 +175,6 @@ public class UserGroup extends SimpleDualControlData<UserGroup>{
 	public String getSuperGroup(){
 	    return this.superGroup;
 	}
-	
 	
 	@Override
 	public void translateToJSON(ParsedJSONContainer jsonContainer) {
@@ -176,71 +187,32 @@ public class UserGroup extends SimpleDualControlData<UserGroup>{
 		jsonContainer.put("applicationId",getApplicationId());
 		jsonContainer.put("createdBy",getCreatedBy());
 		jsonContainer.put("createdOn",getCreatedOn());
-		jsonContainer.put("creatorIPAddress",getCreatorIPAddress());
+//		jsonContainer.put("creatorIPAddress",getCreatorIPAddress());
 		jsonContainer.put("groupCode",getGroupCode());
 		jsonContainer.put("groupName",getGroupName());
 		jsonContainer.put("id",getId());
 		jsonContainer.put("modifiedBy",getModifiedBy());
-		jsonContainer.put("modifiedByIPAddress",getModifiedByIPAddress());
+//		jsonContainer.put("modifiedByIPAddress",getModifiedByIPAddress());
 		jsonContainer.put("modifiedOn",getModifiedOn());
-		jsonContainer.put("activeFlag",getActiveFlag());
+//		jsonContainer.put("activeFlag",getActiveFlag());
 		jsonContainer.put("superGroup",getSuperGroup());
 	}
-	
-	
-	private static final String [] MODIFABLE_FIELDS ={
-		"applicationId", "groupCode","groupName", "superGroup"	,"activeFlag"
-	}; 
-	
-	
+
 	@Override
-	public String[] retrieveModifableFields() {
-		return MODIFABLE_FIELDS;
+	public UserGroup instantiateFromJSON(ParsedJSONContainer jsonContainer) {
+		// TODO Auto-generated method stub
+		return null;
 	}
-	@Override
-	public void setActiveFlag(String activeFlag) {
-		this.activeFlag = activeFlag ; 
-		
-	}
-	@Override
+
 	public String getActiveFlag() {
 		return activeFlag;
 	}
-	@Override
-	public Long getPrimaryKey() {
-		return id;
+
+	public void setActiveFlag(String activeFlag) {
+		this.activeFlag = activeFlag;
 	}
-	@Override
-	public Class<Long> getPrimaryKeyClassType() {
-		return Long.class;
-	}
-	@Override
-	public String getKey1AsString() {
-		return applicationId + "";
-	}
-	@Override
-	public String getKey2AsString() {
-		return groupCode;
-	}
-	@Override
-	public boolean isEraseDataOnApproveErase() {
-		return true;
-	}
-	@Override
-	public String getPrimaryKeyJPAName() {
-		return "id";
-	}
-	@Override
-	protected void extractDataFromJSON(UserGroup targetObject,
-			ParsedJSONContainer jsonContainer) {
-		UserGroup retval = targetObject;
-		retval.setGroupCode( (String)jsonContainer.get("groupCode" ,  String.class.getName()));
-		retval.setGroupName( (String)jsonContainer.get("groupName" ,  String.class.getName()));
-		retval.setId( (Long)jsonContainer.get("id" ,  Long.class.getName()));
-		retval.setModifiedBy( (String)jsonContainer.get("modifiedBy" ,  String.class.getName()));
-		retval.setModifiedByIPAddress( (String)jsonContainer.get("modifiedByIPAddress" ,  String.class.getName()));
-		retval.setModifiedOn( (Date)jsonContainer.get("modifiedOn" ,  Date.class.getName()));
-		retval.setActiveFlag(  (String)jsonContainer.get("activeFlag" ,  String.class.getName()));
-		retval.setSuperGroup( (String)jsonContainer.get("superGroup" ,  String.class.getName()));
-	}
+	
+	
+	
+
 }
