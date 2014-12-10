ddd7d122cb9dacb822c72a7d7964e9a9f9bea0d5
Raka Sanjaya <tubby.po.07@gmail.com>
2014-12-10 14:35:00 +0800

    update code

--- lib-base-server/src/main/java/id/co/sigma/security/server/dao/impl/UserDaoImpl.java
+++ lib-base-server/src/main/java/id/co/sigma/security/server/dao/impl/UserDaoImpl.java
@@ -127,7 +127,7 @@ public class UserDaoImpl extends BaseJPADao implements IUserDao{
 		userRoles = qry.getResultList();
 		
 		Query qryDelegate = getEntityManager().createQuery("from Role r where r.id in (select udr.roleId from UserDelegationRole udr where udr.userDelegateId in ("
-				+ "select ud.id from UserDelegation ud where ud.destUserId=:USER_ID))");
+				+ "select ud.id from UserDelegation ud where ud.destUserId=:USER_ID and ud.dataStatus='A'))");
 		qryDelegate.setParameter("USER_ID", id);
 		delegateRoles = qryDelegate.getResultList();
 		
