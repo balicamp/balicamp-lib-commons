ebeeeff7b2002459645abeab330467246012174f
goesderai <goesderai@gmail.com>
2015-03-04 16:11:25 +0800

    Fix error: "Foreign key (FK_2fhyj7r35lnkb3mi96iwflvmb:m_lookup_details [LOV_ID])) must have same number of columns as the referenced primary key (m_lookup_header [i18_CODE,LOV_ID])"

--- lib-base-server/src/main/java/id/co/sigma/common/server/data/lookup/Common1LevelLOVHeaderPK.java
+++ lib-base-server/src/main/java/id/co/sigma/common/server/data/lookup/Common1LevelLOVHeaderPK.java
@@ -14,13 +14,7 @@ import javax.persistence.Embeddable;
 @Embeddable
 public class Common1LevelLOVHeaderPK implements Serializable {
 	
-	
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = -7711783511851271575L;
-
-
 
 	/**
 	 * column : LOV_ID<br/>
@@ -28,16 +22,6 @@ public class Common1LevelLOVHeaderPK implements Serializable {
 	 **/
 	@Column(name="LOV_ID" )
 	private  String lovId;
-
-	
-	
-	/**
-	 * column : i18_CODE<br/>
-	 * kode i18 dari Lookup
-	 **/
-	@Column(name="i18_CODE")
-	private  String i18Key;
-
 
 	/**
 	 * column : LOV_ID<br/>
@@ -47,7 +31,6 @@ public class Common1LevelLOVHeaderPK implements Serializable {
 		return lovId;
 	}
 
-
 	/**
 	 * column : LOV_ID<br/>
 	 * Id dari lookup
@@ -56,66 +39,40 @@ public class Common1LevelLOVHeaderPK implements Serializable {
 		this.lovId = lovId;
 	}
 
-
-	/**
-	 * column : i18_CODE<br/>
-	 * kode i18 dari Lookup
-	 **/
-	public String getI18Key() {
-		return i18Key;
-	}
-
-
-	/**
-	 * column : i18_CODE<br/>
-	 * kode i18 dari Lookup
-	 **/
-	public void setI18Key(String i18Key) {
-		this.i18Key = i18Key;
-	}
-
-
 	@Override
 	public String toString() {
-		return "Common1LevelLOVHeaderPK [lovId=" + lovId + ", i18Key=" + i18Key
-				+ "]";
+		return "Common1LevelLOVHeaderPK [lovId=" + lovId + "]";
 	}
-
 
 	@Override
 	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((i18Key == null) ? 0 : i18Key.hashCode());
-		result = prime * result + ((lovId == null) ? 0 : lovId.hashCode());
-		return result;
+	    final int prime = 31;
+	    int result = 1;
+	    result = prime * result + ((lovId == null) ? 0 : lovId.hashCode());
+	    return result;
 	}
-
 
 	@Override
 	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Common1LevelLOVHeaderPK other = (Common1LevelLOVHeaderPK) obj;
-		if (i18Key == null) {
-			if (other.i18Key != null)
-				return false;
-		} else if (!i18Key.equals(other.i18Key))
-			return false;
-		if (lovId == null) {
-			if (other.lovId != null)
-				return false;
-		} else if (!lovId.equals(other.lovId))
-			return false;
+	    if (this == obj) {
 		return true;
+	    }
+	    if (obj == null) {
+		return false;
+	    }
+	    if (!(obj instanceof Common1LevelLOVHeaderPK)) {
+		return false;
+	    }
+	    Common1LevelLOVHeaderPK other = (Common1LevelLOVHeaderPK) obj;
+	    if (lovId == null) {
+		if (other.lovId != null) {
+		    return false;
+		}
+	    } else if (!lovId.equals(other.lovId)) {
+		return false;
+	    }
+	    return true;
 	}
-	
-	
-	
 	
 }
 
